01222f1464ef803c98c0069b6e28df09b4dfb8e2
EdouardRaimbault <edouard.raimbault@ensta-paris.fr>
2025-01-17 21:25:43 +0100

    ajout commentaires hill climbing

--- hill_climbing/hill_climbing.py
+++ hill_climbing/hill_climbing.py
@@ -3,25 +3,8 @@ from utilities import calculate_profit, is_feasible
 
 
 def hill_climbing(N, M, initial_solution, resource_consumption, resource_availabilities, profits, generate_neighbors, k):
-    """
-    Implements the hill climbing algorithm for the multidimensional knapsack problem.
-    Optimized with Numba.
-
-    Args:
-        N (int): Number of projects.
-        M (int): Number of resources.
-        initial_solution (np.ndarray): Initial feasible solution vector (array of 0s and 1s).
-        resource_consumption (np.ndarray): Resource consumption matrix (M x N).
-        resource_availabilities (np.ndarray): Array of available resources for each resource type.
-        profits (np.ndarray): Array of profits for each project.
-        generate_neighbors (function): A function to generate neighbors of the current solution.
-        k (int): Degree of neighborhood.
-
-    Returns:
-        np.ndarray: Final solution vector (array of 0s and 1s).
-        float: Total profit of the final solution.
-    """
-    # Initialize the current solution and profit
+    
+    # Initialisation de la solution  courante et du profit associé
     current_solution = initial_solution.copy()
     current_profit = calculate_profit(current_solution, profits)
 
@@ -30,7 +13,7 @@ def hill_climbing(N, M, initial_solution, resource_consumption, resource_availab
         best_neighbor = current_solution
         best_profit = current_profit
 
-        # Evaluate the neighbors
+        # Evaluation des voisins
         for neighbor in neighbors:
             if is_feasible(neighbor, M, resource_consumption, resource_availabilities):
                 neighbor_profit = calculate_profit(neighbor, profits)
@@ -38,11 +21,11 @@ def hill_climbing(N, M, initial_solution, resource_consumption, resource_availab
                     best_neighbor = neighbor
                     best_profit = neighbor_profit
 
-        # If no better neighbor found, stop
+        # Si il n'y a pas de meilleur voisin, stop
         if np.array_equal(current_solution, best_neighbor):
             break
 
-        # Update the solution with the best neighbor
+        # Mise à jour de la solution avec le meilleur voisin
         current_solution = best_neighbor
         current_profit = best_profit
 
